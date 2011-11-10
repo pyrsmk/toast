5e294fe514be6cc64f3870b9a475ce9ab4bb49ed
pyrsmk <dev@dreamysource.fr>
2011-11-10 15:06:31 +0100

    Add nesting and callback support

--- src/toast.js
+++ src/toast.js
@@ -1,7 +1,7 @@
 /*
     toast, just a synchroneous resource loader
 
-    Version     : 0.1.0
+    Version     : 0.2.0
     Author      : Aurélien Delogu (dev@dreamysource.fr)
     Homepage    : https://github.com/pyrsmk/toast
     License     : MIT
@@ -13,29 +13,85 @@
     Parameters
         Array resources
 */
-this.toast=function(resources){
-    if(!(resources instanceof Array)){
-        resources=[resources];
-    }
+this.toast=function(resources,complete,when,callback){
     var resource,
         node,
         doc=document,
-        head=doc.getElementsByTagName('head')[0],
-        i=-1;
+        head=doc.head,
+        createElement='createElement',
+        appendChild='appendChild',
+        addEventListener='addEventListener',
+        i,
+        scriptsToLoad,
+        isComplete=function(resource){
+            if(!--scriptsToLoad){
+                if(complete){
+                    complete();
+                }
+                if(callback){
+                    callback();
+                }
+            }
+        },
+        watchLink=function(node){
+            if(node.textContent || node.text){
+                isComplete();
+            }
+            else{
+                setTimeout(function(){watchLink(node);},250);
+            }
+        }/*,
+        watchObject=function(when){
+            if(window[when]){
+                isComplete();
+            }
+            else{
+                setTimeout(function(){watchObject(when);},250);
+            }
+        },
+        watchScript=function(when){
+            if(when){
+                watchObject(when);
+            }
+            else{
+                isComplete();
+            }
+        }*/;
+    // Format
+    if(resources===''+resources){
+        resources=[resources];
+    }
     // Load resources
-    while(resource=resources[++i]){
-        // Create LINK element
-        if(resource.match(/\.css$/)){
-            node=doc.createElement('link');
+    i=scriptsToLoad=resources.length;
+    while(resource=resources[--i]){
+        // Points out to another resource list
+        if(resource.pop){
+            this.toast(resource[0],resource[1],resource[2],isComplete());
+        }
+        // CSS
+        else if(resource.match(/\.css$/)){
+            // Create LINK element
+            node=doc[createElement]('link');
             node.rel='stylesheet';
             node.href=resource;
-            head.appendChild(node);
+            head[appendChild](node);
+            // Watch loading state
+            watchLink(node);
         }
-        // Create SCRIPT element
+        // JS
         else{
-            node=doc.createElement('script');
+            // Create SCRIPT element
+            node=doc[createElement]('script');
             node.src=resource;
-            head.appendChild(node);
+            head[appendChild](node);
+            // Watch loading state
+            //watchScript(node);
+            if(node[addEventListener]){
+                node[addEventListener]('load',isComplete,false);
+            }
+            else{
+                node.attachEvent('onload',isComplete);
+            }
         }
     }
 };
