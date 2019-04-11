bbecca34db77e7b92b1952cbe2765e7e44ad2479
Aurélien Delogu <dev@dreamysource.fr>
2019-04-11 14:45:53 +0200

    Fix tests on IE9

--- tests/tests.js
+++ tests/tests.js
@@ -1,16 +1,38 @@
+/* global describe, it, expect, toast, universe */
 /* eslint no-var: ["off"] */
 /* eslint func-names: ["off"] */
 /* eslint prefer-arrow-callback: ["off"] */
-/* eslint no-undef: ["off"] */
+/* eslint vars-on-top: ["off"] */
 
+/*
+    Init #test block for CSS tests
+*/
 var div = document.createElement('div')
 div.id = 'test'
 document.querySelector('body').appendChild(div)
 
+/*
+    Detect link.onerror support
+*/
+var link = document.createElement('link')
+var linkOnErrorSupport = false
+link.rel = 'stylesheet'
+link.href = 'notfound'
+link.onerror = function () {
+    linkOnErrorSupport = true
+}
+document.querySelector('head').appendChild(link)
+
+/*
+    Abstract CSS property retrieving
+*/
 function getCss(style) {
     return getComputedStyle(document.querySelector('#test')).getPropertyValue(style)
 }
 
+/*
+    Base: window.toast
+*/
 describe('window.toast', function () {
     it('should exist in window', function () {
         expect(window).to.have.a.property('toast')
@@ -18,9 +40,12 @@ describe('window.toast', function () {
     })
 })
 
+/*
+    toast.css()
+*/
 describe('toast.css()', function () {
     it('should return a promise', function () {
-        expect(toast.css('')).to.be.a('promise')
+        expect(toast.css('')).to.be.an.instanceof(Promise)
     })
     it('should resolve', function (done) {
         toast.css('https://s3.eu-west-3.amazonaws.com/dreamysource/toast/test1-style.css').then(function () {
@@ -31,15 +56,19 @@ describe('toast.css()', function () {
         expect(getCss('margin-top')).to.equal('72px')
     })
     it('should reject', function (done) {
+        if (!linkOnErrorSupport) this.skip()
         toast.css('notfound.css').catch(function () {
             done()
         })
     })
 })
 
+/*
+    toast.js()
+*/
 describe('toast.js()', function () {
     it('should return a promise', function () {
-        expect(toast.js('')).to.be.a('promise')
+        expect(toast.css('')).to.be.an.instanceof(Promise)
     })
     it('should resolve', function (done) {
         toast.js('https://s3.eu-west-3.amazonaws.com/dreamysource/toast/test2-script.js').then(function () {
@@ -56,6 +85,9 @@ describe('toast.js()', function () {
     })
 })
 
+/*
+    toast.load()
+*/
 describe('toast.load()', function () {
     it('should resolve', function (done) {
         toast.load([
@@ -97,6 +129,7 @@ describe('toast.load()', function () {
         })
     })
     it('should reject for a not found CSS file', function (done) {
+        if (!linkOnErrorSupport) this.skip()
         this.timeout(10000)
         toast.load([
             'https://s3.eu-west-3.amazonaws.com/dreamysource/toast/test7-style1.css',
